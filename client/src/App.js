8087f68efa314638085cff5fbc1ac8266c55d5f0
Taran <thetaranneeld@gmail.com>
2019-09-06 10:33:57 -0600

    added component, and basic toggle styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import PlayerList from './components/PlayerList';
+import ColorToggle from './components/ColorToggle';
 
 function App() {
   return (
     <div className='App'>
+      <ColorToggle/>
       <PlayerList/>
     </div>
   );
---
+++ client/src/components/ColorToggle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const ColorToggle = () => {
+    return (
+        <div className="toggle-div">
+            <button className="toggle-btn">Invert color</button>
+        </div>
+    );
+};
+
+export default ColorToggle;
